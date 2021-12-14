d2edead761d5cd5f61d7e595578b2cd5bf38b2be
romanchyla <roman.chyla@gmail.com>
2021-12-13 20:49:01 -0500

    feat: correct (hopefully) impl of floyd all path search

--- cspatterns/dp/shortest_path.py
+++ cspatterns/dp/shortest_path.py
@@ -1,6 +1,99 @@
 from collections import defaultdict, deque
 
 
+class Floyd(object):
+    """
+    All pairs shortest paths with negative weights allowed
+    (but no negative cycles)
+
+    time: O(V^3)
+    space: O(V^2) -- with high constant; as we are keeping 2*V
+           with just mappings from vertex labels back to internal
+           ints (dp); which sucks
+    """
+
+    def __init__(self, graph) -> None:
+        super().__init__()
+        self.graph = graph
+        self._find_shortest_paths()
+
+    def _find_shortest_paths(self):
+
+        # the graph can contain arbitrary labels (not only ints)
+        # and since some of them could have been deleted we can't
+        # rely on the internal int representation; we could have
+        # used map directly - but this will be somewhat better for
+        # large graphs (maybe)
+        V = 0
+        dmap = {}
+        imap = {}
+        for v in self.graph.vertices():
+            dmap[v] = V
+            imap[V] = v
+            V += 1
+
+        dp = [[float("inf")] * V for _ in range(V)]
+        parent = list(range(V))
+
+        for v in range(V):
+            dp[v][v] = 0
+
+        for v, w, weight in self.graph.edges():
+            dp[dmap[v]][dmap[w]] = weight
+            parent[dmap[w]] = parent[dmap[v]]
+
+        for k in range(V):
+            for v in range(V):
+                if v == k or dp[v][k] == float("inf"):
+                    continue
+                for w in range(V):
+                    weight = dp[v][k] + dp[k][w]
+                    if weight < dp[v][w]:
+                        dp[v][w] = weight
+                        parent[w] = k
+
+        self._v2imap = dmap
+        self._i2vmap = imap
+        self._parent = parent
+        self._distances = dp
+
+    def get_distance_between(self, v, w):
+        iv = self._v2imap.get(v, -1)
+        iw = self._v2imap.get(w, -1)
+        if iw == -1 or iv == -1:
+            return None  # one of the vertices is not from the graph
+        return self._distances[iv][iw]
+
+    def get_path_between(self, v, w):
+        d = self.get_distance_between(v, w)
+        if d is None or d == float("inf"):
+            return []
+
+        out = deque()
+        i2vmap = self._i2vmap
+        iv = self._v2imap[v]
+        t = self._v2imap[w]
+        while t != self._parent[t] and t != iv:
+            out.appendleft(i2vmap[t])
+            t = self._parent[t]
+        out.appendleft(v)
+        return list(out)
+
+    def get_path_to(self, w):
+        if not self.graph.has_vertex(w):
+            return []
+
+        out = deque()
+        t = self._v2imap[w]
+        i2vmap = self._i2vmap
+
+        while t != self._parent[t]:
+            out.appendleft(i2vmap[t])
+            t = self._parent[t]
+        out.appendleft(i2vmap[t])
+        return list(out)
+
+
 class BellmannFord(object):
     """
     Find single source shortest path to every vertex in the
@@ -39,8 +132,10 @@ class BellmannFord(object):
         parent = {self.source: None}
 
         for _ in self.graph.vertices():
-            if _ == self.source:
-                continue
+            # WRONG! we must run the cycle V times (ONE MORE TIME than necessary)
+            # if we were to successfuly detect every negative cycle
+            # if _ == self.source:
+            #    continue
             stabilized = True
             for v, w, weight in self.graph.edges():
                 if dst_to[v] + weight < dst_to[w]:
--- tests/test_dp_shortest_paths.py
+++ tests/test_dp_shortest_paths.py
@@ -17,3 +17,25 @@ def test_bellmann_ford():
     assert sp.get_distance_to("t") == 5
     assert sp.get_path_to("t") == ["s", "u", "v", "t"]
     assert sp.get_path_to("x") == []
+
+
+def test_floyd():
+    dg = graphs.WeightedDirectedGraph(
+        ("s", "u", 2),
+        ("s", "v", 4),
+        ("u", "v", -1),
+        ("v", "t", 4),
+        ("u", "w", 2),
+        ("w", "t", 2),
+    )
+    sp = shortest_path.Floyd(dg)
+
+    assert sp.get_distance_between("s", "t") == 5
+    assert sp.get_path_between("s", "t") == ["s", "u", "v", "t"]
+    assert sp.get_path_between("s", "x") == []
+    print(sp._parent, sp._i2vmap)
+    assert sp.get_path_to("t") == ["s", "u", "v", "t"]
+
+
+if __name__ == "__main__":
+    test_floyd()
